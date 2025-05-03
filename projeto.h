fcd02c9c7ab2adf04eb8ce9473dd7222e5c33eb7
João Vitor Escudeiro <joaovitor.escudeiro01@gmail.com>
2025-05-03 09:59:34 -0300

    Definindo a estrutura produto e incluindo bibliotecas

---
+++ projeto.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int codigo;
+    char nome[50];
+    char descricao[100];
+    int quantidade;
+    float preco;
+} Produto;
